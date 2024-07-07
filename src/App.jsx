0b98e18b40da4d39bea7d23912295b53d4039cc3
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-07 22:27:05 +0700

    testing tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-     <h1>haha</h1>
+      <h1 className="text-3xl font-bold text-red-500 underline text-center">haha</h1>
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
