c4a82e7ee2289fec7f73d23180677df652f7c7e9
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-08 00:07:50 +0700

    title changed

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,6 @@ function App() {
     <>
       <Navbar />
       <Header />
-      <h1 className="text-3xl font-bold text-red-500 underline text-center">
-        haha
-      </h1>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
     </>
   );
 }
