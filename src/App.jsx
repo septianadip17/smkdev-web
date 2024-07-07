c30f2a556455cda1d65422086fb9cde522a1a6de
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-07 23:28:20 +0700

    header and navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-red-500 underline text-center">haha</h1>
+      <Navbar />
+      <Header />
+      <h1 className="text-3xl font-bold text-red-500 underline text-center">
+        haha
+      </h1>
       <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
     </>
   );
